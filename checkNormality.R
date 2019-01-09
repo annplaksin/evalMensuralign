8b47e3fe04afb44dd611a6c640b05d9ed2ff8212
annplaksin <annplaksin@gmx.net>
2019-01-09 15:44:30 +0100

    Working normality test script

--- checkNormality.R
+++ checkNormality.R
@@ -2,27 +2,44 @@
 library(pastecs)
 library(ggplot2)
 
-# Load data
+# Try looping to process multiple data files
+for(i in 1:1)
+{
+  # Set filenames
+  input <- paste("normalityInput\\", i, ".csv", sep = "")
+  outQQ <- paste("normalityOutput\\", i, "-QQ.pdf", sep = "")
+  outHist <- paste("normalityOutput\\", i, "-Hist.pdf", sep = "")
+  outStat <- paste("normalityOutput\\", i, "-Stat.txt", sep = "")
+  
+  # Load data
+  data <- read.csv(input, header = TRUE, sep = ";", dec = ",")
+  
+  # Get rid of original alignment results
+  randoms <- subset(data, Source=="randomized")
+  
+  # Calculate relative distance and number of changes
+  randoms$dist.length <- randoms$Distance / randoms$LengthBacktrace
+  randoms$changes.length <- randoms$Number.of.Changes / randoms$LengthBacktrace
+  
+  # Histogram with fitted normality distribution (ggplot2)
+  hist.dist <- ggplot(randoms, aes(dist.length)) + 
+    geom_histogram(aes(y= ..density..), binwidth = 0.004, color = "black", fill = "white") +
+    stat_function(fun = dnorm, args = list(mean = mean(randoms$dist.length), 
+                                           sd = sd(randoms$dist.length)), size = 1) + 
+    labs(y = "Density", x = "Distance (per Alignment Length)")
+  hist.dist + theme_bw()
+  ggsave(outHist, width = 15, height = 10, units = "cm")
+  
+  # Q-Q plot with line (ggplot2)
+  p <- ggplot(randoms, aes(sample = dist.length))
+  p + stat_qq() + stat_qq_line() + theme_bw()
+  ggsave(outQQ, width = 15, height = 10, units = "cm")
+  
+  # pastecs::stat.desc() mit norm = TRUE return beneath a lot of other things skewness, 
+  # kurtosis and shapiro-wilk test
+  stat <- stat.desc(randoms$dist.length, norm = TRUE)
+  
+  # Save results to file
+  write.table(stat, outStat, sep = "\t", row.names = TRUE)
+ }
 
-# Loop: for(i in 1:10){x=x+1; print(x);}
-# bla <- read.csv(filedir)
-
-# Get rid of original alignment results
-
-# Calculate relative distance and number of changes
-
-# Histogram with fitted normality distribution (ggplot2)
-hist.dist <- ggplot(test_Data, aes(Dist.Length)) + 
-  geom_histogram(aes(y= ..density..), binwidth = 0.004, color = "black", fill = "white") +
-  stat_function(fun = dnorm, args = list(mean = mean(test_Data$Dist.Length), sd = sd(test_Data$Dist.Length)), size = 1) + 
-  labs(y = "Density", x = "Distance (per Alignment Length)")
-hist.dist + theme_bw()
-
-# Q-Q plot with line (ggplot2)
-p <- ggplot(test_Data, aes(sample = Dist.Length))
-p + stat_qq() + stat_qq_line()
-
-# pastecs::stat.desc() mit norm = TRUE return beneath a lot of other things skewness, kurtosis and shapiro-wilk test
-stat.desc(data$Dist.Length, norm = TRUE)
-
-# Save results
