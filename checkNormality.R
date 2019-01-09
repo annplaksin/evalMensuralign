83885f616af3da0dbbc3a168eeeab0f8a001a191
annplaksin <annplaksin@gmx.net>
2019-01-09 17:33:16 +0100

    Double loop for files and parameters

--- checkNormality.R
+++ checkNormality.R
@@ -2,44 +2,56 @@
 library(pastecs)
 library(ggplot2)
 
+# Parameter sets
+# parameters <- c("Signbased.vis", "Signbased.log", "Signbased.all", "Superlogical", 
+#                "Signbased.vis.gap", "Signbased.log.gap", "Signbased.all.gap", "Superlogical.gap")
+parameters <- c("Signbased.vis", "Superlogical")
+
+
 # Try looping to process multiple data files
-for(i in 1:1)
+
+# Set i according to number of examples
+for(i in 1:2)
 {
-  # Set filenames
-  input <- paste("normalityInput\\", i, ".csv", sep = "")
-  outQQ <- paste("normalityOutput\\", i, "-QQ.pdf", sep = "")
-  outHist <- paste("normalityOutput\\", i, "-Hist.pdf", sep = "")
-  outStat <- paste("normalityOutput\\", i, "-Stat.txt", sep = "")
-  
-  # Load data
-  data <- read.csv(input, header = TRUE, sep = ";", dec = ",")
-  
-  # Get rid of original alignment results
-  randoms <- subset(data, Source=="randomized")
-  
-  # Calculate relative distance and number of changes
-  randoms$dist.length <- randoms$Distance / randoms$LengthBacktrace
-  randoms$changes.length <- randoms$Number.of.Changes / randoms$LengthBacktrace
-  
-  # Histogram with fitted normality distribution (ggplot2)
-  hist.dist <- ggplot(randoms, aes(dist.length)) + 
-    geom_histogram(aes(y= ..density..), binwidth = 0.004, color = "black", fill = "white") +
-    stat_function(fun = dnorm, args = list(mean = mean(randoms$dist.length), 
-                                           sd = sd(randoms$dist.length)), size = 1) + 
-    labs(y = "Density", x = "Distance (per Alignment Length)")
-  hist.dist + theme_bw()
-  ggsave(outHist, width = 15, height = 10, units = "cm")
-  
-  # Q-Q plot with line (ggplot2)
-  p <- ggplot(randoms, aes(sample = dist.length))
-  p + stat_qq() + stat_qq_line() + theme_bw()
-  ggsave(outQQ, width = 15, height = 10, units = "cm")
-  
-  # pastecs::stat.desc() mit norm = TRUE return beneath a lot of other things skewness, 
-  # kurtosis and shapiro-wilk test
-  stat <- stat.desc(randoms$dist.length, norm = TRUE)
-  
-  # Save results to file
-  write.table(stat, outStat, sep = "\t", row.names = TRUE)
- }
+  # Looping trhough parameters
+  for(p in 1:2)
+  {
+    # Set filenames
+    input <- paste("normalityInput\\", i, "_", parameters[p], ".csv", sep = "")
+    outQQ <- paste("normalityOutput\\", i, "_", parameters[p], "-QQ.pdf", sep = "")
+    outHist <- paste("normalityOutput\\", i, "_", parameters[p], "-Hist.pdf", sep = "")
+    outStat <- paste("normalityOutput\\", i, "_", parameters[p], "-Stat.txt", sep = "")
+    
+    # Load data
+    data <- read.csv(input, header = TRUE, sep = ";", dec = ",")
+    
+    # Get rid of original alignment results
+    randoms <- subset(data, Source=="randomized")
+    
+    # Calculate relative distance and number of changes
+    randoms$dist.length <- randoms$Distance / randoms$LengthBacktrace
+    randoms$changes.length <- randoms$Number.of.Changes / randoms$LengthBacktrace
+    
+    # Histogram with fitted normality distribution (ggplot2)
+    hist.dist <- ggplot(randoms, aes(dist.length)) + 
+      geom_histogram(aes(y= ..density..), binwidth = 0.004, color = "black", fill = "white") +
+      stat_function(fun = dnorm, args = list(mean = mean(randoms$dist.length), 
+                                             sd = sd(randoms$dist.length)), size = 1) + 
+      labs(y = "Density", x = "Distance (per Alignment Length)")
+    hist.dist + theme_bw()
+    ggsave(outHist, width = 15, height = 10, units = "cm")
+    
+    # Q-Q plot with line (ggplot2)
+    p <- ggplot(randoms, aes(sample = dist.length))
+    p + stat_qq() + stat_qq_line() + theme_bw()
+    ggsave(outQQ, width = 15, height = 10, units = "cm")
+    
+    # pastecs::stat.desc() mit norm = TRUE return beneath a lot of other things skewness, 
+    # kurtosis and shapiro-wilk test
+    stat <- stat.desc(randoms$dist.length, norm = TRUE)
+    
+    # Save results to file
+    write.table(stat, outStat, sep = "\t", row.names = TRUE)
+  }
+}
 
