06dec529db5159b7ca4f2bb4492783ef3f7d26e2
annplaksin <annplaksin@gmx.net>
2019-01-12 17:21:23 +0100

    Chi² and Fisher test on influence of parameters on normality of surrogate data

--- normalitySequel.R
+++ normalitySequel.R
@@ -1,11 +1,12 @@
 # Include packages
 library(pastecs)
 library(ggplot2)
+library(gmodels)
 
 
 results <- read.csv2("C:/Users/Rumpel/Source/Repos/R/evalMensuralign/normalityOutput/results.csv")
 
-# Kreuztabellen für reldist.norm < 0.05
+# Subset of reldist.norm < 0.05
 allNotNormal <- subset(results, reldist.norm < 0.05)
 
 # Subsets on sample size
@@ -14,16 +15,12 @@ size900 <- subset(allNotNormal, sample.size == "900")
 size1600 <- subset(allNotNormal, sample.size == "1600")
 size2500 <- subset(allNotNormal, sample.size == "2500")
 
-# Set
+# Easy contingency tables
 bla <- table(allNotNormal$set)
 blubb <- table(allNotNormal$params, allNotNormal$set)
 bloo <- table(allNotNormal$params,allNotNormal$example)
-# Params
 
-
-# Example
-
-
+# Some plots on factors
 plot <- ggplot(allNotNormal, aes(factor(params), fill = factor(example))) + geom_bar()
 plot
 
@@ -32,3 +29,20 @@ plot2
 
 plot3 <- ggplot(allNotNormal, aes(factor(params), fill = factor(set))) + geom_bar()
 plot3
+
+
+# Contigency table on reldist.norm < 0.05 and params
+results$normal <- results$reldist.norm < 0.05
+
+params2normal <- table(results$params, results$normal)
+
+CrossTable(params2normal, chisq = TRUE, expected = TRUE, sresid = TRUE, format = "SPSS")
+# CrossTable with Fisher test throws error, try on better machine
+# CrossTable(params2normal, chisq = TRUE, fisher = TRUE, expected = TRUE, sresid = TRUE, format = "SPSS")
+# Fisher test with simulated p (otherwise workspace too small)
+fisher.test(params2normal, simulate.p.value = TRUE)
+# ... with greater workspace
+fisher.test(params2normal, workspace = 2e+07)
+
+
+# Still need to calculate effect size, because odds ratio works only on 2x2 tables
